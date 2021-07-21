2f42e455e6731793d156b903aff0231277cfd0cc
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2021-07-21 14:44:57 -0500

    Create raspirecibe.py

---
+++ raspi2atmegaReal/raspirecibe.py
@@ -0,0 +1,21 @@
+#con main1.c
+from serial import*
+from time import*
+
+atmega = Serial('/dev/ttyACM0',9600)
+sleep(0.2)
+try:
+ atmega.flushInput()
+ sleep(0.3)
+ while 1:
+  if atmega.in_waiting > 0:
+   mens = atmega.readline().strip()
+   sleep(0.3)
+   print (mens)
+  atmega.flushInput()
+  sleep(3)
+except(KeyboardInterrupt,SystemExit):
+ print("")
+ print("hasta la vista ")
+ atmega.close()
+
