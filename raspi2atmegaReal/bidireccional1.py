2d62288e5fffe1adb2e4c6b8744a68cb4ae29743
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2021-07-21 14:26:36 -0500

    Update bidireccional1.py

--- raspi2atmegaReal/bidireccional1.py
+++ raspi2atmegaReal/bidireccional1.py
@@ -1,24 +1,24 @@
 from serial import*
 from time import*
 
-arduino = Serial('/dev/ttyACM0',9600)
+atmega = Serial('/dev/ttyACM0',9600)
 sleep(2)
 try:
  while 1:
   comando =  raw_input("intro ")
-  arduino.write(comando.encode())
-  arduino.write('\r'.encode())
-  arduino.flushInput()
+  atmega.write(comando.encode())
+  atmega.write('\r'.encode())
+  atmega.flushInput()
   print ("enviado")
   sleep(3)
   try:
    print ("recibiendo datos: ")
-   while not( arduino.in_waiting > 0):
+   while not( atmega.in_waiting > 0):
     print ("datos recibidos")
     sleep (1.5)
     pass
    print ()
-   mens = arduino.readline().strip()
+   mens = atmega.readline().strip()
    print (mens)
   except:
    print("no data recive")
@@ -26,5 +26,5 @@ try:
 except(KeyboardInterrupt,SystemExit):
  print("")
  print("hasta la vista ")
- arduino.close()
+ atmega.close()
 
