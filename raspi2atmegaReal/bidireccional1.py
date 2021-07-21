4b279b8842fe1ca3cd346715b62877c3fc09a042
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2021-07-21 14:03:08 -0500

    Create bidireccional1.py

---
+++ raspi2atmegaReal/bidireccional1.py
@@ -0,0 +1,29 @@
+from serial import*
+from time import*
+
+arduino = Serial('/dev/ttyACM0',9600)
+sleep(2)
+try:
+ while 1:
+  comando =  raw_input("intro ")
+  arduino.write(comando.encode())
+  arduino.write('\r'.encode())
+  arduino.flushInput()
+  print ("enviado")
+  sleep(3)
+  try:
+   print ("nosyes")
+   while not( arduino.in_waiting > 0):
+    print ("yesyes")
+    sleep (1.5)
+    pass
+   mens = arduino.readline().strip()
+   print (mens)
+  except:
+   print("no data recive")
+
+except(KeyboardInterrupt,SystemExit):
+ print("")
+ print("hasta la vista ")
+ arduino.close()
+
