2e3e14c5251ffd4a6ad2bd9ce5dc7a7bb6a134c7
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-12-19 11:20:45 -0500

    Update prueb1.py

--- lcdi2cTT/prueb1.py
+++ lcdi2cTT/prueb1.py
@@ -1,7 +1,7 @@
 import I2C_LCD_driver
 from time import *
 
-I2C_LCD_driver.LCD_begin(0x27)
+I2C_LCD_driver.lcd_begin(0x27)
 mylcd = I2C_LCD_driver.lcd()
 
 mylcd.lcd_display_string("Hola mundo.", 1)
